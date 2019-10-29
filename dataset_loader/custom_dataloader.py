23d09ef88174038358359668ac39071d372f3ea1
gallev898 <gallev898@gmail.com>
2019-10-29 14:46:29 +0200

    /

--- dataset_loader/custom_dataloader.py
+++ dataset_loader/custom_dataloader.py
@@ -9,30 +9,25 @@ from torch.utils.data import Dataset
 
 class Custom_Image_Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, image_path, transform, caption_path=None
-                 ):
-        'Initialization'
+    def __init__(self, image_path, transform, caption_path=None):
         self.image_path = image_path
         self.caption_path = caption_path
         self.transform = transform
-
         self.imgs = []
+
         for f in os.listdir(image_path):
             img = Image.open(os.path.join(image_path, f))
             if self.transform is not None:
                 img = self.transform(img)
 
-            self.imgs.append(img)
+            image_title = f.split('.')[0]
+            self.imgs.append((img, image_title))
 
 
     def __len__(self):
-        'Denotes the total number of samples'
         return len(self.imgs)
 
 
     def __getitem__(self, index):
-        'Generates one sample of data'
-        # Select sample
         img = self.imgs[index]
-
         return img
